4c3807017cda001f10b4a66dea593759a7a578da
StuLiu <1183862787@qq.com>
2019-04-02 21:19:49 +0800

    sss

--- lapsrn_MF_0.py
+++ lapsrn_MF_0.py
@@ -21,76 +21,67 @@ class LapSRN(object):
 	def build_model(self):
 		self.global_step = tf.Variable(tf.constant(0))
 		# tf.train.exponential_decay：使学习率指数衰减
-		self.learn_rate = tf.train.exponential_decay(self.lr,self.global_step,self.Epoch//10,self.dr)	
+		self.learn_rate = tf.train.exponential_decay(self.lr, self.global_step, self.Epoch // 10, self.dr)
 		with tf.variable_scope('input'):
-			self.X = tf.placeholder(tf.float32,[self.batch_size,self.input_size_w,self.input_size_j,7],name = 'x')
-
-			self.Y = tf.placeholder(tf.float32,[self.batch_size,self.output_size_w,self.output_size_j,1],name = 'y')
-
-			#with tf.name_scope('weights'):
-			# self.w是反卷积的时候使用的权重
-			self.w = tf.get_variable(shape = [4,4,1,7],name='w',initializer = tf.ones_initializer())
-			
-			# w_input是卷积时候使用的权重
-			w_input = tf.get_variable(shape = [3,3,7,32],initializer = tf.random_normal_initializer(stddev=np.sqrt(2./(3*3*32))),name='w_input')
+			# 输入低分辨率数据
+			self.X = tf.placeholder(tf.float32, [self.batch_size, self.input_size_w, self.input_size_j, 7], name='x')
+			# 输入低分辨率数据对应的高分辨率标签数据
+			self.Y = tf.placeholder(tf.float32, [self.batch_size, self.output_size_w, self.output_size_j, 1], name='y')
+			# self.w : 超分辨率分支反卷积权重
+			self.w = tf.get_variable(shape=[4, 4, 1, 7], name='w', initializer=tf.ones_initializer())
+			# w_input : 特征提取分支上第一层卷积时候使用的权重，卷积核第三个参数对应X的深度
+			w_input = tf.get_variable(shape=[3, 3, 7, 32],
+			                          initializer=tf.random_normal_initializer(stddev=np.sqrt(2. / (3 * 3 * 32))),
+			                          name='w_input')
 		with tf.variable_scope('weights'):
+			# 特征提取模块，连续五层卷积+一层反卷积+一层卷积
 			for i in range(5):
-				w = tf.get_variable(shape = [3,3,32,32],initializer = tf.random_normal_initializer(stddev=np.sqrt(2./(3*3*32))),name='w'+str(i))
-			#self.w_ = tf.Variable(tf.ones([4,4,32,32]),name='w_')
-			
+				w = tf.get_variable(shape=[3, 3, 32, 32],
+				                    initializer=tf.random_normal_initializer(stddev=np.sqrt(2. / (3 * 3 * 32))),
+				                    name='w' + str(i))
 			# self.w_是一系列卷积后再进行反卷积使用的卷积核  4 4是卷积核的尺寸，1是输出通道数，32是输入通道数
-			self.w_ = tf.get_variable(shape = [4,4,1,32],initializer = tf.ones_initializer(),name='w_')
-			#bi_filter = get_upsample_filter(4,16)
-			#self.w_ = tf.Variable(tf.cast(bi_filter,tf.float32),name='w_')
-			#w_out = tf.Variable(tf.random_normal([3,3,32,1],stddev = np.sqrt(2./(3*3*1))),name='w_out')
-			
+			self.w_ = tf.get_variable(shape=[4, 4, 1, 32], initializer=tf.ones_initializer(), name='w_')
 			# w_out是进行先卷积再反卷积之后再SAME卷积作为输出时使用的卷积核
-			w_out = tf.get_variable(shape = [3,3,1,1],initializer = tf.random_normal_initializer(stddev = np.sqrt(2./(3*3*1))),name='w_out')
+			w_out = tf.get_variable(shape=[3, 3, 1, 1],
+			                        initializer=tf.random_normal_initializer(stddev=np.sqrt(2. / (3 * 3 * 1))),
+			                        name='w_out')
 
-		#self.I = tf.nn.relu(tf.nn.conv2d_transpose(self.X,self.w,output_shape=[self.batch_size,self.output_size,self.output_size,1],strides=[1,2,2,1],padding='SAME'))
-		
-		#tf.nn.conv2d_transpose一共有5个参数：分别为：1、指需要做反卷积的输入图像，它要求是一个Tensor
-	    # 2、卷积核，它要求是一个Tensor，具有[filter_height, filter_width, out_channels, in_channels]这样的shape，具体含义是[卷积核的高度，卷积核的宽度，卷积核个数，图像通道数]
+		# tf.nn.conv2d_transpose一共有5个参数：分别为：1、指需要做反卷积的输入图像，它要求是一个Tensor
+		# 2、卷积核，它要求是一个Tensor，具有[filter_height, filter_width, out_channels, in_channels]这样的shape，具体含义是[卷积核的高度，卷积核的宽度，卷积核个数，图像通道数]
 		# 3、反卷积操作输出的shape。4、反卷积时在图像每一维的步长，这是一个一维的向量，长度4。5、string类型的量，只能是"SAME","VALID"其中之一，这个值决定了不同的卷积方式
 		##-----------------------------------------------------------------------------------##
-		# 先使用反卷积把输入的数据提高到label大小。此时以变成单通道
-		self.I = tf.nn.conv2d_transpose(self.X,self.w,output_shape=[self.batch_size,self.output_size_w,self.output_size_j,1],strides=[1,5,5,1],padding='SAME')
-		# 接着使用Leaky_relu激活函数进行激活，
-		self.I = tf.maximum(0.1*self.I,self.I)# realize the leaky_relu function 
-		#conv_input = tf.nn.relu(tf.nn.conv2d(self.X,w_input,strides=[1,1,1,1],padding='SAME'))
-		##-----------------------------------------------------------------------------------##
-		
-		# 使用w_input对输入进行SAME卷积，并使用Leaky_relu激活函数进行激活
-		conv_input = tf.nn.conv2d(self.X,w_input,strides=[1,1,1,1],padding='SAME')
-		conv_input = tf.maximum(0.1*conv_input,conv_input)
-		
-		with tf.variable_scope('weights',reuse=True):
-			for i in range(5):
-				w = tf.get_variable('w'+str(i))
-				#conv_input = tf.nn.relu(tf.nn.conv2d(conv_input,w,strides=[1,1,1,1],padding='SAME'))
-				# 分别获取w_i,对conv_input进行SAME卷积操作，使用Leaky_relu激活函数进行激活
-				conv_input = tf.nn.conv2d(conv_input,w,strides=[1,1,1,1],padding='SAME')
-				conv_input = tf.maximum(conv_input*0.2,conv_input,name = 'conv'+str(i))
-		#conv_10 = tf.nn.relu(tf.nn.conv2d_transpose(conv10,self.w_,output_shape=[self.batch_size,self.output_size,self.output_size,32],strides=[1,2,2,1],padding='SAME'))
-		#conv_10 = tf.nn.relu(tf.nn.conv2d_transpose(conv_input,self.w_,output_shape=[self.batch_size,self.output_size,self.output_size,1],strides=[1,2,2,1],padding='SAME'))
-		
-		# 最后使用反卷积操作，对conv_input进行上采样到output_size大小。这里使用的权重为self.w_，使用Leaky_relu激活函数进行激活
-		conv_10 = tf.nn.conv2d_transpose(conv_input,self.w_,output_shape=[self.batch_size,self.output_size_w,self.output_size_j,1],strides=[1,5,5,1],padding='SAME')
-		conv_10 = tf.maximum(conv_10*0.1,conv_10)
-		
-		# 反卷积之后，再次进行SAME卷积，作为特征提取分支的输出。
-		#self.R = tf.nn.relu(tf.nn.conv2d(conv_10,w_out,strides=[1,1,1,1],padding='SAME'))
-		self.R = tf.nn.conv2d(conv_10,w_out,strides=[1,1,1,1],padding='SAME')
-		self.R = tf.maximum(self.R*0.1,self.R)
+		# 超分辨率分支：使用反卷积把输入的数据提高到label大小，此时以变成单通道。并使用Leaky_relu激活函数进行激活
+		self.I = tf.nn.conv2d_transpose(self.X, self.w,
+		                                output_shape=[self.batch_size, self.output_size_w, self.output_size_j, 1],
+		                                strides=[1, 5, 5, 1], padding='SAME')
+		self.I = tf.maximum(0.1 * self.I, self.I)  # realize the leaky_relu function
 
-		# 将上述两部分输出相加，作为最终的输出。
+		# 特征提取分支第一步：使用w_input对输入进行SAME卷积，并使用Leaky_relu激活函数进行激活
+		conv_input = tf.nn.conv2d(self.X, w_input, strides=[1, 1, 1, 1], padding='SAME')
+		conv_input = tf.maximum(0.1 * conv_input, conv_input)
+		# 特征提取分支第二步：五层SAME卷积
+		with tf.variable_scope('weights', reuse=True):
+			for i in range(5):
+				w = tf.get_variable('w' + str(i))
+				conv_input = tf.nn.conv2d(conv_input, w, strides=[1, 1, 1, 1], padding='SAME')
+				conv_input = tf.maximum(conv_input * 0.2, conv_input, name='conv' + str(i))
+		# 特征提取分支第三步：使用反卷积操作，对conv_input进行上采样到output_size大小。这里使用的权重为self.w_，使用Leaky_relu激活函数进行激活
+		conv_10 = tf.nn.conv2d_transpose(conv_input, self.w_,
+		                                 output_shape=[self.batch_size, self.output_size_w, self.output_size_j, 1],
+		                                 strides=[1, 5, 5, 1], padding='SAME')
+		conv_10 = tf.maximum(conv_10 * 0.1, conv_10)
+		# 特征提取分支第四步：反卷积之后，再次进行SAME卷积，作为特征提取分支的输出。
+		self.R = tf.nn.conv2d(conv_10, w_out, strides=[1, 1, 1, 1], padding='SAME')
+		self.R = tf.maximum(self.R * 0.1, self.R)
+
+		# 合并操作：将两个分支的输出相加，作为最终的输出。
 		self.O = self.I + self.R
-		self.O = tf.nn.dropout(self.O,self.keep_prob)
-		
+		self.O = tf.nn.dropout(self.O, self.keep_prob)
+
 		# 分别计算：损失、学习到的残差、真实的残差
-		self.loss = tf.reduce_mean(tf.sqrt(tf.square(self.Y-self.O)+1e-6))
-		self.rl = tf.reduce_mean(tf.abs(self.R)+1e-6)
-		self.rr = tf.reduce_mean(tf.abs(self.Y-self.I)+1e-6)
+		self.loss = tf.reduce_mean(tf.sqrt(tf.square(self.Y - self.O) + 1e-6))
+		self.rl = tf.reduce_mean(tf.abs(self.R) + 1e-6)
+		self.rr = tf.reduce_mean(tf.abs(self.Y - self.I) + 1e-6)
 		self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
 		self.saver = tf.train.Saver()
